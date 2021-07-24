d40454b8f2216a7a39c71028fbd5f3b8a98105ac
jomas94 <jomariojsa@gmail.com>
2021-07-24 21:31:03 +0200

    added assets

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
@@ -23,7 +24,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
